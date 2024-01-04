87c161027033491e92860fd39f866ac960273547
Maritza Aulia Savitri <maritzaauliasavitri@Maritzas-MacBook-Air.local>
2024-01-04 17:44:31 +0700

    fifth

--- js/script.js
+++ js/script.js
@@ -5,21 +5,7 @@ function hitungluas(){
 }
 
 function hitungkeliling(){
-    sisi1=+document.getElementById("input-sisi1").value;
-    sisi2=+document.getElementById("input-sisi2").value;
-    sisi3=+document.getElementById("input-sisi3").value;
-    keliling = sisi1 + sisi2 + sisi3;
-    document.getElementById('keliling').value = keliling;}
-
-function resetForm1(){
-    document.getElementById("luas").textContent=" ";
-    document.getElementById("input-alas").textContent="input";
-    document.getElementById("input-tinggi").textContent=" ";
-}
-
-function resetForm2(){
-    document.getElementById("keliling").textContent=" ";
-    document.getElementById("input-sisi3").textContent=" ";
-    document.getElementById("input-sisi2").textContent=" ";
-    document.getElementById("input-sisi1").textContent=" ";
-}
+    const sisi1 = document.getElementById("sisi1").value;
+    const sisi2 = document.getElementById("sisi2").value;
+    const sisi3 = document.getElementById("sisi3").value;
+   document.getElementById('keliling').value = sisi1 + sisi2 + sisi3;}
