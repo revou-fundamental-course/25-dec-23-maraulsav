58e83844064c5d9df97b151a6a3000a9bd4a8bad
DeffreusTheda <thedadeffreus@gmail.com>
2024-01-04 09:34:12 +0700

    Error fix & code polish

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 function hitungluas(){
-    alas=document.getElementById("alas").value;
-    tinggi=document.getElementById("tinggi").value;
-    luas = 1/2*alas*tinggi;
-   document.getElementById('luas').value = luas;}
-
+    const alas   = document.getElementById("alas").value;
+    const tinggi = document.getElementById("tinggi").value;
+   document.getElementById('luas').value = 0.5*alas*tinggi;
+}
 function hitungkeliling(){
-    sisi1=+document.getElementById("sisi1").value;
-    sisi2=+document.getElementById("sisi2").value;
-    sisi3=+document.getElementById("sisi3").value;
-    keliling = sisi1 + sisi2 + sisi3;
-   document.getElementById('keliling').value = keliling;}
+    const sisi1 = document.getElementById("sisi1").value;
+    const sisi2 = document.getElementById("sisi2").value;
+    const sisi3 = document.getElementById("sisi3").value;
+   document.getElementById('keliling').value = sisi1 + sisi2 + sisi3;
+}
