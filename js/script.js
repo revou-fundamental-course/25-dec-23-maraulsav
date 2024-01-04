0029a7ccd94fe3b9cc30ddc3f798c06889118b26
Maritza Aulia Savitri <maritzaauliasavitri@Maritzas-MacBook-Air.local>
2024-01-04 17:50:31 +0700

    Sixth push

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ function hitungluas(){
     const tinggi = document.getElementById("tinggi").value;
    document.getElementById('luas').value = 0.5*alas*tinggi;
 }
-
 function hitungkeliling(){
-    const sisi1 = document.getElementById("sisi1").value;
-    const sisi2 = document.getElementById("sisi2").value;
-    const sisi3 = document.getElementById("sisi3").value;
-   document.getElementById('keliling').value = sisi1 + sisi2 + sisi3;}
+    const sisi1 = +document.getElementById("sisi1").value;
+    const sisi2 = +document.getElementById("sisi2").value;
+    const sisi3 = +document.getElementById("sisi3").value;
+   document.getElementById('keliling').value = sisi1 + sisi2 + sisi3;
+}
