b817ccfb8f82a729fd3d66abece31726c3c9a4bc
Maritza Aulia Savitri <maritzaauliasavitri@Maritzas-MacBook-Air.local>
2024-01-04 10:24:48 +0700

    second

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 function hitungluas(){
-    alas=document.getElementById("alas").value;
-    tinggi=document.getElementById("tinggi").value;
+    alas=document.getElementById("input-alas").value;
+    tinggi=document.getElementById("input-tinggi").value;
     luas = 1/2*alas*tinggi;
    document.getElementById('luas').value = luas;}
 
 function hitungkeliling(){
-    sisi1=+document.getElementById("sisi1").value;
-    sisi2=+document.getElementById("sisi2").value;
-    sisi3=+document.getElementById("sisi3").value;
+    sisi1=+document.getElementById("input-sisi1").value;
+    sisi2=+document.getElementById("input-sisi2").value;
+    sisi3=+document.getElementById("input-sisi3").value;
     keliling = sisi1 + sisi2 + sisi3;
    document.getElementById('keliling').value = keliling;}
+
+function resetForm1(){
+    document.getElementById("luas").textContent=" ";
+    document.getElementById("input-alas").textContent="input";
+    document.getElementById("input-tinggi").textContent=" ";
+}
+
+function resetForm2(){
+    document.getElementById("keliling").textContent=" ";
+    document.getElementById("input-sisi3").textContent=" ";
+    document.getElementById("input-sisi2").textContent=" ";
+    document.getElementById("input-sisi1").textContent=" ";
+}
