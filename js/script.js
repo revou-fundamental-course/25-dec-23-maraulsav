19da3763c4a3a41a2be3ca10fbb2415a33b26fe1
DeffreusTheda <thedadeffreus@gmail.com>
2024-01-05 21:58:56 +0700

    Bug Fix

--- js/script.js
+++ js/script.js
@@ -7,5 +7,5 @@ function hitungkeliling(){
     const sisi1 = +document.getElementById("sisi1").value;
     const sisi2 = +document.getElementById("sisi2").value;
     const sisi3 = +document.getElementById("sisi3").value;
-   document.getElementById('keliling').value = sisi1 + sisi2 + sisi3;
+   document.getElementById('keliling').value = +sisi1 + +sisi2 + +sisi3;
 }
